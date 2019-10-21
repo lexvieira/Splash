99ce762d083c6fb9e60bf28e04b74b845bd2b1b8
Alex Vieira <lexvieira.info@gmail.com>
2019-10-21 06:52:35 -0700

    Last changes include the option for reload the form

--- frmSplash.Designer.cs
+++ frmSplash.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace Splash
+﻿using System;
+
+namespace Splash
 {
     partial class frmSplash
     {
@@ -28,37 +30,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmSplash));
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.picAdvertising = new System.Windows.Forms.PictureBox();
+            this.lblAlert = new System.Windows.Forms.Label();
+            this.btnReload = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picAdvertising)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // picAdvertising
             // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(799, 447);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.picAdvertising.Location = new System.Drawing.Point(0, 0);
+            this.picAdvertising.Name = "picAdvertising";
+            this.picAdvertising.Size = new System.Drawing.Size(799, 447);
+            this.picAdvertising.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picAdvertising.TabIndex = 0;
+            this.picAdvertising.TabStop = false;
+            // 
+            // lblAlert
+            // 
+            this.lblAlert.AutoSize = true;
+            this.lblAlert.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAlert.ForeColor = System.Drawing.Color.Crimson;
+            this.lblAlert.Location = new System.Drawing.Point(47, 219);
+            this.lblAlert.Name = "lblAlert";
+            this.lblAlert.Size = new System.Drawing.Size(706, 25);
+            this.lblAlert.TabIndex = 1;
+            this.lblAlert.Text = "Verifique se a imagem com o nome Propaganda está na mesma pasta do App ;)";
+            this.lblAlert.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblAlert.Visible = false;
+            // 
+            // btnReload
+            // 
+            this.btnReload.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReload.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.btnReload.Location = new System.Drawing.Point(328, 247);
+            this.btnReload.Name = "btnReload";
+            this.btnReload.Size = new System.Drawing.Size(145, 23);
+            this.btnReload.TabIndex = 2;
+            this.btnReload.Text = "Recarregar Imagem";
+            this.btnReload.UseVisualStyleBackColor = true;
+            this.btnReload.Visible = false;
+            this.btnReload.Click += new System.EventHandler(this.BtnReload_Click);
             // 
             // frmSplash
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnReload);
+            this.Controls.Add(this.lblAlert);
+            this.Controls.Add(this.picAdvertising);
             this.Name = "frmSplash";
             this.Text = "Splash Screen";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResizeEnd += new System.EventHandler(this.FrmSplash_ResizeEnd);
+            ((System.ComponentModel.ISupportInitialize)(this.picAdvertising)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox picAdvertising;
+        private System.Windows.Forms.Label lblAlert;
+        private System.Windows.Forms.Button btnReload;
     }
 }
 
--- frmSplash.cs
+++ frmSplash.cs
@@ -12,9 +12,41 @@ namespace Splash
 {
     public partial class frmSplash : Form
     {
+        string appDirectory = AppDomain.CurrentDomain.BaseDirectory + "propaganda.jpg";
         public frmSplash()
         {
             InitializeComponent();
+
+            verifyAdvertising();
+
+        }
+
+        private void FrmSplash_ResizeEnd(object sender, EventArgs e)
+        {
+            this.picAdvertising.Size = this.ClientSize;
+            this.lblAlert.Width = this.Width;
+            this.lblAlert.Location = new Point((this.ClientSize.Width / 2) - (this.lblAlert.Width / 2), (this.ClientSize.Height / 2) - ((this.lblAlert.Height / 2)+30));
+            this.btnReload.Location = new Point((this.ClientSize.Width / 2) - (this.btnReload.Width/2), (this.ClientSize.Height/2) - (this.btnReload.Height / 2));
+        }
+
+        private void BtnReload_Click(object sender, EventArgs e)
+        {
+            verifyAdvertising();
+        }
+
+        private void verifyAdvertising() {
+            if (System.IO.File.Exists(appDirectory))
+            {
+                this.picAdvertising.ImageLocation = appDirectory;
+                this.lblAlert.Visible = false;
+                this.btnReload.Visible = false;
+            }
+            else
+            {
+                this.lblAlert.Visible = true;
+                this.btnReload.Visible = true;
+            }
+
         }
     }
 }
